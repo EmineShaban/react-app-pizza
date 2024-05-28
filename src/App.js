a60eca784b88a409e46d5c1810c4182dba5f551b
Emine Shaban <emineshaban@icloud.com>
2024-05-28 21:18:29 +0300

    .

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import {Cart, Home} from './pages'
 import { Route, Routes } from 'react-router';
 
 function App() {
+  
+  const [pizzas, setPizzas] = React.useState([]);
+  console.log(pizzas)
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json').then((resp) => resp.json()).then(json =>{
+      setPizzas(json.pizzas)
+    })
+  }, [])
+
   return (
     <div className="wrapper">
       <Header/>
     <div className="content">
       <Routes>
-    <Route path='/' Component={Home} exact/>
+    <Route path='/' element={<Home items={pizzas} />}  exact/>
     <Route path='/cart' Component={Cart} exact/>
     </Routes>
     </div>
