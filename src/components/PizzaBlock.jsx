1c0259d589cc66bba49a41ec4753326cbf8b5101
Emine Shaban <emineshaban@icloud.com>
2024-05-28 21:25:34 +0300

    .

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 function PizzaBlock({name, imageUrl, price}) {
+    const types = ['тонкое', 'традиционное'];
+    const [activeType, setActiveType] = React.useState(0);
+    const onSelectType = (index) =>{
+        setActiveType(index) 
+    }
+
+
   return (
     <div className="pizza-block">
     <img
@@ -11,8 +18,14 @@ function PizzaBlock({name, imageUrl, price}) {
     <h4 className="pizza-block__title">{name}</h4>
     <div className="pizza-block__selector">
         <ul>
-            <li className="active">тонкое</li>
-            <li>традиционное</li>
+
+            {types.map((type, index) => (
+                <li onClick={() => onSelectType(index)}
+                key={index}
+                className={activeType === index ? 'active' : ''}>{type}</li>
+            ))}
+            {/* <li className="active">тонкое</li> */}
+            
         </ul>
         <ul>
             <li className="active">26 см.</li>
